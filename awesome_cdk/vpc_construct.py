765aa9735c24b882df03d8f68038f1967793e36c
leon <leonaugusto16@gmail.com>
2019-10-15 23:25:22 -0300

    update organization vpc

--- awesome_cdk/vpc_construct.py
+++ awesome_cdk/vpc_construct.py
@@ -4,11 +4,17 @@ from aws_cdk import (
 )
 
 class VpcConstruct(core.Construct):
-
     def __init__(self, scope: core.Construct, id: str) -> None:
         super().__init__(scope, id)
-        self._vpc = ec2.Vpc(self, 'AwesomeVPC', cidr='10.0.0.0/21', max_azs=3, 
-                    subnet_configuration=[ec2.SubnetConfiguration(name='PrivateName', subnet_type=ec2.SubnetType.PRIVATE, cidr_mask=24), ec2.SubnetConfiguration(name='PublicName', subnet_type=ec2.SubnetType.PUBLIC, cidr_mask=24)])
+        self._vpc = ec2.Vpc(
+            self, 'AwesomeVPC',
+            cidr='10.0.0.0/21',
+            max_azs=3, 
+            subnet_configuration=[
+                ec2.SubnetConfiguration(name='PrivateName',subnet_type=ec2.SubnetType.PRIVATE, cidr_mask=24),
+                ec2.SubnetConfiguration(name='PublicName', subnet_type=ec2.SubnetType.PUBLIC,cidr_mask=24)
+            ]
+        )
     @property
     def vpc(self):
         return tuple(self._vpc)
