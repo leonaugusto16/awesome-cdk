edafddae89ebf95f089368799c54338004af24f9
leon <leonaugusto16@gmail.com>
2020-06-17 14:51:21 -0300

    add configs in bin

--- bin/awesome-cdk.ts
+++ bin/awesome-cdk.ts
@@ -4,4 +4,10 @@ import * as cdk from '@aws-cdk/core';
 import { AwesomeCdkStack } from '../lib/awesome-cdk-stack';
 
 const app = new cdk.App();
-new AwesomeCdkStack(app, 'AwesomeCdkStack');
+new AwesomeCdkStack(app, 'AwesomeCdkStack', {
+    env:{
+        account: process.env.CDK_DEFAULT_ACCOUNT,
+        region: process.env.CDK_DEFAULT_REGION,
+    },
+    stackName: 'AwesomeCdkStack'
+});
