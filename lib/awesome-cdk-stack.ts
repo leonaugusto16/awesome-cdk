e19b1499a7718035fad436f4b5d0600d439541cd
leon <leonaugusto16@gmail.com>
2020-06-17 14:42:49 -0300

    eks simple poc

--- lib/awesome-cdk-stack.ts
+++ lib/awesome-cdk-stack.ts
@@ -1,9 +1,44 @@
 import * as cdk from '@aws-cdk/core';
+import iam = require('@aws-cdk/aws-iam');
+import ec2 = require('@aws-cdk/aws-ec2');
+
+import { EksCluster } from './eks-cluster'
 
 export class AwesomeCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const cidr = "10.0.0.0/16"
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'TestVpc', {
+      cidr
+    })
+    const clusterAdmin = new iam.Role(this, 'AdminRole', {
+      assumedBy: new iam.AccountRootPrincipal()
+    });
+
+    const instanceRole = new iam.Role(this, 'instanceRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+    instanceRole.addToPolicy(new iam.PolicyStatement({
+      actions: ["ec2:DescribeInstances", "ec2:CreateTags", "ec2:DescribeTags"],
+      resources: ['*'],
+    }));
+
+    const sg = new ec2.SecurityGroup(this, "Sg", {
+      vpc,
+      allowAllOutbound: true,
+      description: 'General security group define nodes',
+      securityGroupName: 'sg-nodes-test'
+    });
+
+    sg.addIngressRule(ec2.Peer.ipv4(cidr), ec2.Port.allUdp(), "Configs Port");
+    sg.addIngressRule(ec2.Peer.ipv4(cidr), ec2.Port.allTcp(), "Configs Port");
+    sg.addIngressRule(ec2.Peer.ipv4(cidr), ec2.Port.allIcmp(), "Configs Port");
+
+    new EksCluster(this,'EKS-Cluster',{
+      vpc,
+      clusterRole: clusterAdmin,
+      nodeRole: instanceRole
+    })
   }
 }
---
+++ lib/eks-cluster.ts
@@ -0,0 +1,34 @@
+import * as cdk from '@aws-cdk/core';
+import ec2 = require('@aws-cdk/aws-ec2');
+import iam = require('@aws-cdk/aws-iam');
+import eks = require('@aws-cdk/aws-eks');
+
+export interface EksProps {
+  vpc: ec2.IVpc,
+  clusterRole: iam.IRole,
+  nodeRole: iam.IRole
+}
+export class EksCluster extends cdk.Construct {
+  constructor(scope: cdk.Construct, id: string, props: EksProps) {
+    super(scope, id);
+    
+    // now define the cluster and map role to "masters" RBAC group
+    const cluster = new eks.Cluster(this, 'ClusterCD', {
+        mastersRole: props.clusterRole,
+        vpc: props.vpc,
+        defaultCapacity: 0
+    });
+
+    cluster.addNodegroup('NodeGroup', {
+      instanceType: new ec2.InstanceType('t2.medium'),
+      nodegroupName: 'NodeGroupCD',
+      desiredSize: 3, 
+      minSize: 2,
+      forceUpdate: true,
+      labels: {
+        "node": "node-group-cd"
+      }
+    });
+
+  }
+}
